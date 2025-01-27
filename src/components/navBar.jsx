abc163a4d439a5cc095305cc6bf23eac140b4251
Viren-Kevat <viren0210@gmail.com>
2025-01-28 01:53:16 +0530

    nav bar

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
+import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
@@ -8,11 +9,14 @@ import Drawer from "@mui/material/Drawer";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Button from "@mui/material/Button";
 
 const Navbar = () => {
   const [drawerOpen, setDrawerOpen] = useState(false);
   const [showNavbar, setShowNavbar] = useState(true); // Control navbar visibility
   const [lastScrollY, setLastScrollY] = useState(0); // Track scroll position
+  const isMobile = useMediaQuery("(max-width: 768px)"); // Detect if screen width is 768px or less
 
   const toggleDrawer = (open) => () => {
     setDrawerOpen(open);
@@ -44,21 +48,17 @@ const Navbar = () => {
     <div>
       {/* Conditionally render the AppBar if showNavbar is true */}
       {showNavbar && (
-        <AppBar position="fixed" sx={{ backgroundColor: "#1976d209" }}>
-          <Toolbar>
-            {/* Burger Menu Icon */}
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              onClick={toggleDrawer(true)}
-            >
-              <MenuIcon />
-            </IconButton>
+        <AppBar
+          position="fixed"
+          sx={{
+            backgroundImage:
+              "linear-gradient(45deg, rgba(255, 255, 255, 0.28), rgba(0, 123, 255, 0.33), rgba(255, 20, 145, 0.25))",
+          }}
+        >
+          <Toolbar sx={{ justifyContent: "space-between" }}>
             <Typography
               variant="h6"
               sx={{
-                flexGrow: 1,
                 display: "flex",
                 justifyContent: "center", // Center horizontally
                 alignItems: "center", // Center vertically
@@ -67,81 +67,179 @@ const Navbar = () => {
             >
               Portfolio
             </Typography>
+            {isMobile ? (
+              <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                onClick={toggleDrawer(true)}
+              >
+                <MenuIcon />
+              </IconButton>
+            ) : (
+              <div>
+                <Button
+                  color="inherit"
+                  href="#home"
+                  sx={{
+                    "&:hover": {
+                      color: "#000000", // Change to black color
+                      textDecoration: "underline",
+                      transition: "color 0.3s ease-in-out", // Smooth transition
+                      fontWeight: "bold", // Make text bold
+                    },
+                  }}
+                >
+                  Home
+                </Button>
+                <Button
+                  color="inherit"
+                  href="#hero"
+                  sx={{
+                    "&:hover": {
+                      color: "#000000", // Change to black color
+                      textDecoration: "underline",
+                      transition: "color 0.3s ease-in-out", // Smooth transition
+                      fontWeight: "bold", // Make text bold
+                    },
+                  }}
+                >
+                  Hero
+                </Button>
+                <Button
+                  color="inherit"
+                  href="#about"
+                  sx={{
+                    "&:hover": {
+                      color: "#000000", // Change to black color
+                      textDecoration: "underline",
+                      transition: "color 0.3s ease-in-out", // Smooth transition
+                      fontWeight: "bold", // Make text bold
+                    },
+                  }}
+                >
+                  About
+                </Button>
+                <Button
+                  color="inherit"
+                  href="#skills"
+                  sx={{
+                    "&:hover": {
+                      color: "#000000", // Change to black color
+                      textDecoration: "underline",
+                      transition: "color 0.3s ease-in-out", // Smooth transition
+                      fontWeight: "bold", // Make text bold
+                    },
+                  }}
+                >
+                  Skills
+                </Button>
+                <Button
+                  color="inherit"
+                  href="#projects"
+                  sx={{
+                    "&:hover": {
+                      color: "#000000", // Change to black color
+                      textDecoration: "underline",
+                      transition: "color 0.3s ease-in-out", // Smooth transition
+                      fontWeight: "bold", // Make text bold
+                    },
+                  }}
+                >
+                  Projects
+                </Button>
+                <Button
+                  color="inherit"
+                  href="#contact"
+                  sx={{
+                    "&:hover": {
+                      color: "#000000", // Change to black color
+                      textDecoration: "underline",
+                      transition: "color 0.3s ease-in-out", // Smooth transition
+                      fontWeight: "bold", // Make text bold
+                    },
+                  }}
+                >
+                  Contact
+                </Button>
+              </div>
+            )}
           </Toolbar>
         </AppBar>
       )}
-
       {/* Drawer for Burger Menu */}
-      <Drawer
-        anchor="left"
-        open={drawerOpen}
-        onClose={toggleDrawer(false)}
-        sx={{
-          "& .MuiDrawer-paper": {
-            backgroundColor: "#ffffff00", // Set a transparent background
-            backdropFilter: "blur(10px)", // Apply the blur effect
-            WebkitBackdropFilter: "blur(10px)", // For Safari support
-            color: "#ffffff",
-          },
-        }}
-      >
-        <List sx={{ width: 250 }}>
-          <ListItem button>
-            <a
-              href="#home"
-              onClick={() => setDrawerOpen(false)}
-              style={{ textDecoration: "none", color: "#ffffff" }} // Use style here
-            >
-              <ListItemText primary="Home" />
-            </a>
-          </ListItem>
-          <ListItem button>
-            <a
-              href="#hero"
-              onClick={() => setDrawerOpen(false)}
-              style={{ textDecoration: "none", color: "#ffffff" }} // Use style here
-            >
-              <ListItemText primary="Hero" />
-            </a>
-          </ListItem>
-          <ListItem button>
-            <a
-              href="#about"
-              onClick={() => setDrawerOpen(false)}
-              style={{ textDecoration: "none", color: "#ffffff" }} // Use style here
-            >
-              <ListItemText primary="About" />
-            </a>
-          </ListItem>
-          <ListItem button>
-            <a
-              href="#skills"
-              onClick={() => setDrawerOpen(false)}
-              style={{ textDecoration: "none", color: "#ffffff" }} // Use style here
-            >
-              <ListItemText primary="Skills" />
-            </a>
-          </ListItem>
-          <ListItem button>
-            <a
-              href="#projects"
-              onClick={() => setDrawerOpen(false)}
-              style={{ textDecoration: "none", color: "#ffffff" }} // Use style here
-            >
-              <ListItemText primary="Projects" />
-            </a>
-          </ListItem>
-          <ListItem button>
-            <a
-              href="#contact"
-              onClick={() => setDrawerOpen(false)}
-              style={{ textDecoration: "none", color: "#ffffff" }} // Use style here
-            >
-              <ListItemText primary="Contact" />
-            </a>
-          </ListItem>
-        </List>
-      </Drawer>
+      {isMobile && (
+        <Drawer
+          anchor="right"
+          open={drawerOpen}
+          onClose={toggleDrawer(false)}
+          sx={{
+            "& .MuiDrawer-paper": {
+              backgroundColor: "#ffffff00", // Set a transparent background
+              backdropFilter: "blur(10px)", // Apply the blur effect
+              WebkitBackdropFilter: "blur(10px)", // For Safari support
+              color: "#ffffff",
+            },
+          }}
+        >
+          <List sx={{ width: 250 }}>
+            <ListItem button>
+              <a
+                href="#home"
+                onClick={() => setDrawerOpen(false)}
+                style={{ textDecoration: "none", color: "#ffffff" }} // Use style here
+              >
+                <ListItemText primary="Home" />
+              </a>
+            </ListItem>
+            <ListItem button>
+              <a
+                href="#hero"
+                onClick={() => setDrawerOpen(false)}
+                style={{ textDecoration: "none", color: "#ffffff" }} // Use style here
+              >
+                <ListItemText primary="Hero" />
+              </a>
+            </ListItem>
+            <ListItem button>
+              <a
+                href="#about"
+                onClick={() => setDrawerOpen(false)}
+                style={{ textDecoration: "none", color: "#ffffff" }} // Use style here
+              >
+                <ListItemText primary="About" />
+              </a>
+            </ListItem>
+            <ListItem button>
+              <a
+                href="#skills"
+                onClick={() => setDrawerOpen(false)}
+                style={{ textDecoration: "none", color: "#ffffff" }} // Use style here
+              >
+                <ListItemText primary="Skills" />
+              </a>
+            </ListItem>
+            <ListItem button>
+              <a
+                href="#projects"
+                onClick={() => setDrawerOpen(false)}
+                style={{ textDecoration: "none", color: "#ffffff" }} // Use style here
+              >
+                <ListItemText primary="Projects" />
+              </a>
+            </ListItem>
+            <ListItem button>
+              <a
+                href="#contact"
+                onClick={() => setDrawerOpen(false)}
+                style={{ textDecoration: "none", color: "#ffffff" }} // Use style here
+              >
+                <ListItemText primary="Contact" />
+              </a>
+            </ListItem>
+          </List>
+        </Drawer>
+      )}
     </div>
   );
 };
